b364cd964f683ff37e53da50d61933c02418c487
Joseph Hunkeler <jhunk@stsci.edu>
2016-04-26 09:58:08 -0400

    Warren fixed outstanding problems. Adjusting build scripts for jwst*

--- jwst_pipeline/build.sh
+++ jwst_pipeline/build.sh
@@ -1,8 +1,5 @@
 
 pip install --no-deps --force --upgrade d2to1
-
-rm -rf jwst_pipeline/timeconversion
-
 for d in jwst_pipeline/*
 do
 ( cd $d && python setup.py install || exit 1 )
--- jwst_tools/build.sh
+++ jwst_tools/build.sh
@@ -1,13 +1,6 @@
 
 pip install --no-deps --force --upgrade d2to1
-
-rm -rf jwst_tools/timeconversion
-
 for d in jwst_tools/*
 do
-    if [[ $d == *spectools* ]]; then
-        # Indentation errors in package
-        continue
-    fi
     ( cd $d && python setup.py install || exit 1 )
 done

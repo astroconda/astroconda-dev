d2f66bbcf50fb3899e3124a90ec25e8306eb4ca6
Joseph Hunkeler <jhunk@stsci.edu>
2016-05-02 15:13:11 -0400

    Implement request to disable jwst_lib.{modeling,wcs}

--- jwst_lib/build.sh
+++ jwst_lib/build.sh
@@ -2,5 +2,11 @@
 pip install --no-deps --force --upgrade d2to1
 for d in jwst_lib/*
 do
-( cd $d && python setup.py install || exit 1 )
+    case "$d" in
+        # Append deprecated packages here to disable them
+        */modeling) continue ;;
+        */wcs) continue ;;
+        *) echo "Processing package: $d" ;;
+    esac
+    ( cd $d && python setup.py install || exit 1 )
 done

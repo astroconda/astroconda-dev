0387d301493eaf0491e8590264a1e3f257c6e90f
Joseph Hunkeler <jhunkeler@gmail.com>
2016-06-16 09:01:32 -0400

    Make headerpadding more portable

--- jwst/build.sh
+++ jwst/build.sh
@@ -1,2 +1,12 @@
-LDFLAGS="-headerpad_max_install_names"
+case `uname` in
+    Darwin)
+    export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
+    ;;
+    Linux)
+    export CFLAGS="${CFLAGS} -Wl,-headerpad_max_install_names"
+    ;;
+    *)
+    ;;
+esac
+
 $PYTHON setup.py install

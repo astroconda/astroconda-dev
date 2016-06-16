a8f9ab7f50ae99c94fba11d27f34391711dd188a
Joseph Hunkeler <jhunkeler@gmail.com>
2016-06-16 09:30:02 -0400

    Convert case to one-liner

--- jwst/build.sh
+++ jwst/build.sh
@@ -1,12 +1,2 @@
-case `uname` in
-    Darwin)
-    export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
-    ;;
-    Linux)
-    export CFLAGS="${CFLAGS} -Wl,-headerpad_max_install_names"
-    ;;
-    *)
-    ;;
-esac
-
+export CFLAGS="${CFLAGS} -Wl,-headerpad_max_install_names"
 $PYTHON setup.py install

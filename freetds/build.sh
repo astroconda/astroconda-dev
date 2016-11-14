f5075e7321d8a89c0b7ddf92c649b9e7fefe0acf
Joseph Hunkeler <jhunk@stsci.edu>
2016-11-14 15:15:14 -0500

    Ensure linkage is 64-bit

--- freetds/build.sh
+++ freetds/build.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ $ARCH -gt 32 ]; then
+    if [[ `uname` == Linux ]]; then
+        export LDFLAGS="-L/usr/lib64"
+    fi
+fi
+
 ./configure --prefix=${PREFIX} --enable-krb5
 make -j${CPU_COUNT}
 make install -k || true

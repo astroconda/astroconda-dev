f6c4f9a7895e05b3f7d703248b163e494cea8be1
Matt Rendina <mrendina@stsci.edu>
2017-08-11 15:31:26 -0400

    Add platform-selective CFLAGS var for POSIX compliance on MacOS

--- cfitsio/build.sh
+++ cfitsio/build.sh
@@ -1,3 +1,7 @@
-
+case "$(uname)" in
+    Darwin)
+        export CFLAGS="$CFLAGS -D_POSIX_C_SOURCE=200112L"
+        ;;
+esac
 ./configure --prefix=$PREFIX --disable-static --enable-reentrant
 (make -j $CPU_COUNT shared && make install) || exit 1

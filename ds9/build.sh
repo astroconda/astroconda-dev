9dc4cc085d2075810766b712a8d82a67ad1c80a0
Joseph Hunkeler <jhunkeler@users.noreply.github.com>
2017-08-13 22:17:40 -0400

    [ds9] Disable xscreensaver support (#50)
    * Disable xscreensaver support
    
    * Update meta.yaml

--- ds9/build.sh
+++ ds9/build.sh
@@ -1,20 +1,18 @@
-
 case "$(uname)" in
-Linux)
-LDFLAGS='-m64'
-;;
-Darwin)
-LDFLAGS='-arch x86_64 -m64'
-
-sed -i -e '/codesign/d' ds9/unix/Makefile.in
-;;
-*)
-echo "Unsupported"
-exit 1
-;;
+    Linux)
+        LDFLAGS='-m64'
+    ;;
+    Darwin)
+        LDFLAGS='-arch x86_64 -m64'
+        sed -i -e '/codesign/d' ds9/unix/Makefile.in
+    ;;
+    *)
+        echo "Unsupported"
+        exit 1
+    ;;
 esac
 
-unix/configure
+unix/configure TKFLAGS="--disable-xss"  # not all machines have libXss
 make
 mkdir -p $PREFIX/bin
 cp -a bin/ds9* bin/x* $PREFIX/bin

34885eea71088fe680b398df2ab18013dc1c1579
Joseph Hunkeler <jhunkeler@users.noreply.github.com>
2017-09-20 14:31:57 -0400

    Make sed calls portable (#69)

--- cubeviz/build.sh
+++ cubeviz/build.sh
@@ -1,3 +1,9 @@
 # Replace malformed pytest requirement with something sane
-sed -i .bad -e 's/pytest==*/pytest>=3.1.0/' setup.py setup.cfg
+pytest_pattern='s/pytest==*/pytest>=3.1.0/'
+for f in setup.*
+do
+    sed -e ${pytest_pattern} "${f}" > "${f}.new"
+    mv "${f}.new" "${f}"
+done
+
 $PYTHON setup.py install

597a17a194c0f8445a9d232730cc29564fcc0654
Joseph Hunkeler <jhunkeler@gmail.com>
2017-12-21 14:19:49 -0500

    Add missing curly braces

--- cubeviz/build.sh
+++ cubeviz/build.sh
@@ -5,7 +5,7 @@ numpy_pattern='s|numpy>=1.13|numpy>=1.12|'
 
 for f in setup.*
 do
-    sed -e ${pytest_pattern} -e $glue_pattern -e $numpy_pattern "${f}" > "${f}.new"
+    sed -e ${pytest_pattern} -e ${glue_pattern} -e ${numpy_pattern} "${f}" > "${f}.new"
     mv "${f}.new" "${f}"
 done
 

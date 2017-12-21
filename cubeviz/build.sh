d5fa8e9327fe35398b8d236a1747225b5070d9d8
Joseph Hunkeler <jhunkeler@gmail.com>
2017-12-21 14:15:27 -0500

    cubeviz: sed patching glue/numpy/pytest

--- cubeviz/build.sh
+++ cubeviz/build.sh
@@ -1,8 +1,11 @@
 # Replace malformed pytest requirement with something sane
-pytest_pattern='s/pytest==*/pytest>=3.1.0/'
+pytest_pattern='s/pytest==/pytest>=/'
+glue_pattern='s/glueviz//'
+numpy_pattern='s|numpy>=1.13|numpy>=1.12|'
+
 for f in setup.*
 do
-    sed -e ${pytest_pattern} "${f}" > "${f}.new"
+    sed -e ${pytest_pattern} -e $glue_pattern -e $numpy_pattern "${f}" > "${f}.new"
     mv "${f}.new" "${f}"
 done
 

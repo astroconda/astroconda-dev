6515ef3f0fbf3b9077f9b17bcf16135c86a15671
Joseph Hunkeler <jhunkeler@gmail.com>
2017-12-21 19:38:42 -0500

    Remove d2to1 dep

--- imexam/bld.bat
+++ imexam/bld.bat
@@ -1,7 +1,2 @@
-
-pip install --no-deps --upgrade --force d2to1
-if errorlevel 1 exit 1
-pip install --no-deps --upgrade --force astropy_helpers || exit 1
-if errorlevel 1 exit 1
-python setup.py install --offline
+python setup.py install
 if errorlevel 1 exit 1

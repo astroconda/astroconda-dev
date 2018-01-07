6afc55b7ac65e68f1c5b621d9b16fedc7efc23ab
Joseph Hunkeler <jhunk@stsci.edu>
2018-01-07 11:13:16 -0500

    Force linkage against Intel OpenMP

--- sextractor/build.sh
+++ sextractor/build.sh
@@ -1,8 +1,11 @@
+rm -rf aclocal.m4 autom4te.cache
+libtoolize -f -i
 autoreconf -i
+
 ./configure --prefix=$PREFIX \
     --disable-static \
     --enable-mkl-gcc \
     --with-mkl-dir=$PREFIX
 
-make -j ${CPU_COUNT}
+make -j ${CPU_COUNT} LDFLAGS=-liomp5
 make install

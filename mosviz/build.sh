0dd1e8b6ecd97bf8f8663328e515afa115736320
Joseph Hunkeler <jhunkeler@users.noreply.github.com>
2018-02-01 11:38:59 -0500

    Drop specutils 0.2.2 version pinning in setup (#104)

--- mosviz/build.sh
+++ mosviz/build.sh
@@ -1,3 +1,6 @@
+sed 's|specutils==0.2.2|specutils|' < setup.cfg > setup.cfg.new
+cp setup.cfg.new setup.cfg
+
 # Remove reference to glueviz from setup script, since this breaks the build
 # as there is no file containing 'glueviz' produced for that package since
 # glueviz has become a metapackage.
--- specviz/build.sh
+++ specviz/build.sh
@@ -1,1 +1,4 @@
+sed 's|specutils==0.2.2|specutils|' < setup.cfg > setup.cfg.new
+cp setup.cfg.new setup.cfg
+
 $PYTHON setup.py install

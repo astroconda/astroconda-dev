00cd8b1aaac3d5a43a0ca66541194ea7942576fa
Joseph Hunkeler <jhunkeler@gmail.com>
2018-08-01 12:02:08 -0400

    Help prevent linkage against conda libcurl

--- cfitsio/build.sh
+++ cfitsio/build.sh
@@ -1,8 +1,8 @@
 case "$(uname)" in
     Darwin)
         export CFLAGS="$CFLAGS -D_POSIX_C_SOURCE=200112L"
-        ;;
+    ;;
 esac
-./configure --prefix=$PREFIX --disable-static --enable-reentrant
-make -j $CPU_COUNT shared
+./configure --prefix=$PREFIX --enable-reentrant
+make shared
 make install

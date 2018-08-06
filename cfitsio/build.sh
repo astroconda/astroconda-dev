80331fc973573336871f7048f5c50150bd013d77
Joseph Hunkeler <jhunkeler@gmail.com>
2018-08-06 16:05:18 -0400

    Apply patch to recipe

--- cfitsio/build.sh
+++ cfitsio/build.sh
@@ -3,6 +3,6 @@ case "$(uname)" in
         export CFLAGS="$CFLAGS -D_POSIX_C_SOURCE=200112L"
     ;;
 esac
-./configure --prefix=$PREFIX --enable-reentrant
+./configure --prefix=$PREFIX --enable-reentrant --disable-curl
 make shared
 make install
